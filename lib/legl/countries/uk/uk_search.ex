f4219bc5f809930f9b1017b28591609949753b0c
shotleybuilder <drjasonwoodruff@gmail.com>
2023-03-20 16:05:08 +0000

    improved api for SEARCH

--- lib/legl/countries/uk/uk_search.ex
+++ lib/legl/countries/uk/uk_search.ex
@@ -27,6 +27,79 @@ defmodule Legl.Countries.Uk.UkSearch do
   @parser &Legl.Services.LegislationGovUk.Parsers.ParserSearch.search_parser/1
   @at_csv "airtable_new_law?"
 
+  @climate_change [
+    ~w[
+      climate\u00a0change
+      energy\u00a0conservation
+      sustainable\u00a0energy
+      greenhouse\u00a0gas
+      ozone\u00a0depleting
+      ozone-depleting
+    ]
+  ]
+
+  @general [
+    ~w[
+      environmental\u00a0impact\u00a0assessment
+    ],
+    ~w[
+      circular\u00a0economy
+    ]
+  ]
+
+  @marine [
+    ~w[
+      marine\u00a0pollution
+      marine\u00a0conservation
+      fish\u00a0conservation
+      deep\u00a0sea\u00a0mining
+      eels
+    ],
+    ~w[
+      coastal\u00a0access
+    ],
+    ~w[
+      river\u00a0pollution
+      river\u00a0conservation
+    ]
+  ]
+
+  @pollution [
+    ~w[
+      control\u00a0of\u00a0pollution
+      oil\u00a0pollution
+      pollution\u00a0prevention
+      nitrate\u00a0pollution
+      prevention\u00a0of\u00a0pollution
+    ]
+  ]
+
+  @waste [
+    ~w[
+      waste\u00a0management
+      special\u00a0waste
+      hazardous\u00a0waste
+      waste\u00a0incineration
+      landfill
+      list\u00a0of\u00a0waste
+      shipment\u00a0of\u00a0waste
+      waste\u00a0electrical
+      packaging\u00a0waste
+      controlled\u00a0waste
+    ],
+    ~w[
+      contaminated\u00a0land
+    ]
+  ]
+
+  @water [
+    ~w[
+      water\u00a0abstraction
+      water\u00a0pollution
+      discharge\u00a0consent
+    ]
+  ]
+
   @wildlife_countryside [
     ~w[
       countryside
@@ -66,6 +139,19 @@ defmodule Legl.Countries.Uk.UkSearch do
     ]
   ]
 
+  def search_terms() do
+    %{
+      search_type: :title,
+      climate_change: @climate_change,
+      general: @general,
+      marine: @marine,
+      pollution: @pollution,
+      waste: @waste,
+      water: @water,
+      wildlife_countryside: @wildlife_countryside
+    }
+  end
+
   @pattern quote do: [
     {"td", _, [{"a", [{"href", _}], [var!(title)]}]},
     {"td", _, [{"a", [{"href", var!(path)}], [var!(_year_number)]}]},
@@ -75,13 +161,23 @@ defmodule Legl.Countries.Uk.UkSearch do
   @welsh quote do: [
     {"td", [{"class", "bilingual cy"}], [{"a", [{"href", var!(_path)}, _], [var!(_title)]}]}]
 
-
-  def run() do
+  @doc """
+    To run the saved searches use
+      Legl.Countries.Uk.UkSearch.run(:waste)
+    To search inside the content
+      Legl.Countries.Uk.UkSearch.run(:waste, %{search_type: :all})
+    To run a tailored search
+      Legl.Countries.Uk.UkSearch.run(:waste, %{waste: [["waste"]], search_type: :title})
+  """
+  def run(search, opts \\ []) do
+    opts = Enum.into(opts, search_terms())
+    #IO.inspect(opts)
+    search_set = Map.get(opts, search) #|> IO.inspect()
     {:ok, file} = "lib/#{@at_csv}.csv" |> Path.absname() |> File.open([:utf8, :write])
-    IO.puts(file, "Name,Title_EN,type,year,number,search")
+    IO.puts(file, "Name,Title_EN,type_code,year,number,search")
     results =
-      Enum.reduce(@wildlife_countryside, %{}, fn search_terms, acc ->
-        workflow(search_terms, acc)
+      Enum.reduce(search_set, %{}, fn search_terms, acc ->
+        workflow(search_terms, opts.search_type, acc)
       end)
     Enum.each(results, fn {_k, v} ->
       save_to_csv(file, v)
@@ -89,17 +185,23 @@ defmodule Legl.Countries.Uk.UkSearch do
     File.close(file)
   end
 
-  def workflow(search_terms, results) do
+  def workflow(search_terms, search_type, results) do
     Enum.reduce(search_terms, results, fn search, acc ->
       search = Regex.replace(~r/\s/u, search, " ")
       IO.puts("#{search}")
-      get_search_results(search, acc)
+      url =
+        case search_type do
+          :title ->
+            URI.encode(~s[/all?title="#{search}"&results-count=1000&sort=year])
+          _ ->
+            URI.encode(~s[/all?text="#{search}"&results-count=1000&sort=year])
+        end
+      get_search_results(url, search, acc)
     end)
   end
 
-  def get_search_results(search, results) do
+  def get_search_results(url, search, results) do
     with(
-      url = URI.encode(~s[/all?text="#{search}"&results-count=1000&sort=year]),
       IO.puts("#{url}"),
       {:ok, table_data} <- RecordGeneric.leg_gov_uk_html(url, @client, @parser),
       {:ok, results} <- process_search_table(table_data, search, results)
